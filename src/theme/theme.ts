98d280bb725b018aba936f3cb5cca10b98c04bb1
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-10 02:43:26 -0500

    fix theme components

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,14 @@
-import { extendTheme, theme as baseTheme, type ThemeConfig } from '@chakra-ui/react';
+import {
+  extendTheme,
+  theme as baseTheme,
+  type ThemeConfig,
+} from '@chakra-ui/react';
 import * as components from './components';
 import * as foundations from './foundations';
 
-
 export const theme: ThemeConfig = extendTheme({
+  ...baseTheme,
   ...foundations,
-  components,
+  components: { ...components },
   colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
 });
