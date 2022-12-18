6b718d823b4a62b0f73e7e6dc5279bf185c34c14
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-17 22:12:02 -0500

    update trpc HOC config

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -4,15 +4,16 @@ import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
 import superjson from 'superjson';
 
 import { type AppRouter } from '../server/trpc/router/_app';
+import { DEFAULT_STALE_TIME, PORT } from './constants';
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  return `http://localhost:${(PORT || process.env.SERVER_PORT) ?? 3000}`; // dev SSR should use localhost
 };
 
 export const trpc = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
     return {
       transformer: superjson,
       links: [
@@ -21,12 +22,29 @@ export const trpc = createTRPCNext<AppRouter>({
             process.env.NODE_ENV === 'development' ||
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
+        httpBatchLink({ url: `${getBaseUrl()}/api/trpc` }),
       ],
+      queryClientConfig: {
+        defaultOptions: { queries: { staleTime: DEFAULT_STALE_TIME } },
+      },
+      // To use SSR properly you need to forward the client's headers to the server
+      headers: () => {
+        if (ctx?.req) {
+          const headers = ctx?.req?.headers;
+          delete headers?.connection;
+          return {
+            ...headers,
+            'x-ssr': '1',
+          };
+        }
+        return {};
+      },
     };
   },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  // display all requests in brwoser network tab, if true requests only shown in server console
   ssr: false,
 });
 
