fcc6767dc8c16e2e9d6d983bc3ce1e47b0e78a45
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-19 16:40:16 -0500

    WIP: cypress config -- ts-issue

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
     // setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    retries: {
+      runMode: 3,
+    },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false,
+    screenshotOnRunFailure: false,
   },
 });
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress"/>
+
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000');
