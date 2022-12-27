0535e40a211d118e1f43302a5c16bfd69fb35683
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-26 23:00:36 -0500

    add helper fns

--- src/utils/fns.ts
+++ src/utils/fns.ts
@@ -62,3 +62,56 @@ export function getAnonId() {
   if (!isClient) return undefined;
   return localStorage.getItem('__anon_id');
 }
+
+/**
+ * * @SEE: https://stackoverflow.com/a/20728864
+ *
+ * @export
+ * @param {number} [end=12]
+ * @return {*}
+ */
+export function generateRandomString(end = 12) {
+  return Math.random().toString(36).substring(3, end);
+}
+
+export const getSearchQuery = (searchParams: URLSearchParams) => {
+  const query: Record<string, string> = {};
+  const entries = searchParams.entries();
+  for (const [key, value] of entries) query[key] = value;
+  return query;
+};
+
+export const composeUrl = (url: string, params: Record<string, any>) => {
+  const composedUrl = new URL(url);
+  return new URL(
+    `${composedUrl.origin}${composedUrl.pathname}?${new URLSearchParams([
+      ...Array.from(composedUrl.searchParams.entries()),
+      ...Object.entries(params),
+    ])}`
+  );
+};
+
+export const numberFormater = (number: number) =>
+  new Intl.NumberFormat().format(number);
+
+export const currencyFormatter = (value: number, currency = 'NGN') => {
+  return new Intl.NumberFormat('en-NG', { style: 'currency', currency }).format(
+    value
+  );
+};
+
+export function addDays(date: Date, days: number): Date {
+  // https://stackoverflow.com/questions/563406/how-to-add-days-to-date
+  const result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+export function dateToSeconds(datestamp: Date): number {
+  const dateSecondsAsFloat = new Date(datestamp).getTime() / 1000;
+  return Math.floor(dateSecondsAsFloat);
+}
+
+export function secondsToDate(seconds: number) {
+  return new Date(seconds * 1000);
+}
