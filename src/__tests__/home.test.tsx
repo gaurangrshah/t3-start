602823e1cbd8325d59f512f02127a2046b50dff8
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-27 01:45:22 -0500

    test: homepage

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,22 +1,47 @@
-import Home from '@/pages/index';
+import { Button } from '@chakra-ui/react';
+import * as nextAuth from 'next-auth/react';
 
+import Home from '@/pages/index';
 import { render, screen, userEvent } from '@/tests/utils';
 
-// const nextAuth = jest.mock('next-auth');
+// jest.mock('next-auth/react');
+const nextAuthMocked = nextAuth as jest.Mocked<typeof nextAuth>;
+const { signIn } = nextAuthMocked;
 
-// const { signIn } = nextAuth;
+export const mockSession = {
+  expires: '1',
+  user: { id: '1', email: 'a', name: 'Delta', image: 'c' },
+};
 
 describe('homepage', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   test('it renders', async () => {
-    const user = userEvent.setup();
     render(<Home />);
+
     const heading = await screen.getByRole('heading', {
       name: /create t3 app/i,
     });
-    const btn = await screen.getByRole('button', { name: /sign in/i });
-
     expect(heading).toBeInTheDocument();
+
+    const btn = await screen.getByRole('button', { name: /sign in/i });
+    expect(btn).toBeInTheDocument();
+  });
+
+  test('it clicks', async () => {
+    const user = userEvent.setup();
+    // const signIn = jest.fn();
+    const spy = jest
+      .spyOn(nextAuthMocked, 'signIn')
+      .mockImplementation(() => mockSession);
+    render(<Button onClick={() => signIn()}>Sign In</Button>);
+
+    const btn = await screen.getByRole('button', { name: /sign in/i });
     expect(btn).toBeInTheDocument();
     user.click(btn);
+
+    // expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
   });
 });
