4a2f13802c5c976e41d76bc8380a13729d7a8e4b
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-29 12:30:14 -0500

    implement trpc error formatter inspired by Bison

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,12 +1,40 @@
 import { initTRPC, TRPCError } from '@trpc/server';
+import { TRPC_ERROR_CODE_KEY } from '@trpc/server/rpc';
 import superjson from 'superjson';
 
 import { type Context } from './context';
 
+export class AppError extends TRPCError {
+  // @NOTE: AppError config inspired by BisonApp
+  invalidArgs?: Record<string, any>;
+  constructor(data: {
+    message?: string;
+    code: TRPC_ERROR_CODE_KEY;
+    cause?: unknown;
+    invalidArgs?: Record<string, any>;
+  }) {
+    const { invalidArgs, ...superData } = data;
+    super(superData);
+
+    this.invalidArgs = invalidArgs;
+  }
+}
+
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  // errorFormatter({ shape }) {
+  //   return shape;
+  // },
+  // @NOTE: ErrorFormatter config inspired by BisonApp
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        invalidArgs: error instanceof AppError ? error.invalidArgs : undefined,
+        message: error.message,
+      },
+    };
   },
 });
 
@@ -33,7 +61,25 @@ const isAuthed = t.middleware(({ ctx, next }) => {
   });
 });
 
+const adminMiddleware = t.middleware(({ ctx, next }) => {
+  // @NOTE Inspired by BisonApp
+  // @TODO: implement user role and check for role === 'admin' instead of the condition below
+  if (!ctx.session || !ctx.session.user || !ctx.session.expires) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      // infers that `user` is non-nullable to downstream procedures
+      user: ctx.session.user,
+    },
+  });
+});
+
 /**
- * Protected procedure
+ * Protected procedures
  **/
 export const protectedProcedure = t.procedure.use(isAuthed);
+export const adminProcedure = protectedProcedure.use(adminMiddleware); // this may not work see next
+// export const adminProcedure = t.procedure(adminMiddleware)
