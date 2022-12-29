c441d53ba2951594d0d9e06f63f2d717ab32026a
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-28 22:24:16 -0500

    test: update home / pages

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -2,7 +2,15 @@ import { Button } from '@chakra-ui/react';
 
 import { signIn, signOut } from '@/lib/next-auth';
 import Home, { SignInButton } from '@/pages/index';
-import { render, screen, userEvent } from '@/tests/utils';
+import {
+  render,
+  renderHook,
+  screen,
+  userEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+  wrapper,
+} from '@/tests/utils';
 import { trpc } from '@/utils/trpc';
 
 // jest.mock('next-auth/react', () => {
@@ -31,17 +39,28 @@ import { trpc } from '@/utils/trpc';
 //   jest.clearAllMocks();
 // });
 
+function useCustomHook() {
+  return trpc.example.hello.useQuery({ text: 'from test' });
+}
+
 describe('homepage', () => {
+  // test('should load example query results', async () => {
+  //   const { result } = renderHook(() => useCustomHook());
+  // });
+
   test('it renders', async () => {
     render(<Home />);
 
     const heading = await screen.getByRole('heading', {
+      level: 1,
       name: /create t3 app/i,
     });
     expect(heading).toBeInTheDocument();
 
-    const subtitle = await screen.getByText(/loading trpc query.../i);
+    const subtitle = await screen.queryByText(/loading trpc query.../i);
     expect(subtitle).toBeInTheDocument();
+
+    // await waitForElementToBeRemoved(() => subtitle);
 
     const btn = await screen.getByRole('button', { name: /sign in/i });
     expect(btn).toBeInTheDocument();
@@ -74,13 +93,12 @@ describe('homepage', () => {
     expect(btn).toBeInTheDocument();
     user.click(btn);
   });
+
   test('should click', async () => {
     const user = userEvent.setup();
     render(<Home />);
     const btn = await screen.getByRole('button', { name: /sign in/i });
     expect(btn).toBeInTheDocument();
     user.click(btn);
-
-    // expect(signIn).toHaveBeenCalled()
   });
 });
---
+++ src/__tests__/pages.test.tsx
@@ -0,0 +1,194 @@
+import Home from '@/pages/index';
+import { render, screen } from '@/tests/utils';
+
+describe('it renders', () => {
+  test('home', async () => {
+    render(<Home />);
+
+    const main = await screen.getByRole('main');
+
+    expect(main).toMatchInlineSnapshot(`
+      .emotion-0 {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        min-height: 100vh;
+        background-image: linear-gradient(to bottom, #2e026d, #15162c);
+      }
+
+      .emotion-1 {
+        text-align: center;
+      }
+
+      .emotion-2 {
+        color: var(--chakra-colors-white);
+      }
+
+      .emotion-4 {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-flex-direction: column;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        gap: 0.5rem;
+      }
+
+      .emotion-5 {
+        color: var(--chakra-colors-white);
+        font-size: var(--chakra-fontSizes-2xl);
+      }
+
+      .emotion-6 {
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-flex-direction: column;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        gap: var(--chakra-space-4);
+      }
+
+      .emotion-8 {
+        display: -webkit-inline-box;
+        display: -webkit-inline-flex;
+        display: -ms-inline-flexbox;
+        display: inline-flex;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -ms-appearance: none;
+        appearance: none;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        position: relative;
+        white-space: nowrap;
+        vertical-align: middle;
+        outline: 2px solid transparent;
+        outline-offset: 2px;
+        line-height: 1.2;
+        border-radius: var(--chakra-radii-full);
+        font-weight: var(--chakra-fontWeights-medium);
+        transition-property: var(--chakra-transition-property-common);
+        transition-duration: var(--chakra-transition-duration-normal);
+        height: var(--chakra-sizes-10);
+        min-width: var(--chakra-sizes-10);
+        font-size: var(--chakra-fontSizes-md);
+        -webkit-padding-start: var(--chakra-space-4);
+        padding-inline-start: var(--chakra-space-4);
+        -webkit-padding-end: var(--chakra-space-4);
+        padding-inline-end: var(--chakra-space-4);
+        color: var(--chakra-colors-white);
+        background: rgba(255, 255, 255, 0.1);
+        padding: 0.75rem 2.5rem;
+      }
+
+      .emotion-8:focus-visible,
+      .emotion-8[data-focus-visible] {
+        box-shadow: var(--chakra-shadows-outline);
+      }
+
+      .emotion-8:disabled,
+      .emotion-8[disabled],
+      .emotion-8[aria-disabled=true],
+      .emotion-8[data-disabled] {
+        opacity: 0.4;
+        cursor: not-allowed;
+        box-shadow: var(--chakra-shadows-none);
+      }
+
+      .emotion-8:hover,
+      .emotion-8[data-hover] {
+        background: rgba(255, 255, 255, 0.2);
+      }
+
+      .emotion-8:focus:not(:focus-visible) {
+        box-shadow: var(--chakra-shadows-none);
+      }
+
+      .emotion-8[aria-checked=true],
+      .emotion-8[data-checked] {
+        background: var(--chakra-colors-gray-100);
+      }
+
+      .emotion-8:active,
+      .emotion-8[data-active] {
+        background: var(--chakra-colors-gray-100);
+      }
+
+      <main
+        class="emotion-0"
+      >
+        <div
+          class="emotion-1"
+        >
+          <h1
+            class="emotion-2"
+          >
+            Create 
+            <span
+              class="emotion-2"
+            >
+              T3
+            </span>
+             App
+          </h1>
+          <div
+            class="emotion-4"
+          >
+            <p
+              class="emotion-5"
+            >
+              Loading tRPC query...
+            </p>
+            <div
+              class="emotion-6"
+            >
+              <p
+                class="emotion-5"
+              />
+              <button
+                class="chakra-button emotion-8"
+                type="button"
+              >
+                Sign in
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+    `);
+  });
+});
