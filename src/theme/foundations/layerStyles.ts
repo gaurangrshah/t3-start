57cda8a1b1583633e1ef40d85f648f49219f5b6c
gshah2020 <gaurang.r.shah@gmail.com>
2022-12-31 02:59:27 -0500

    basic form utils + setup

--- src/theme/foundations/layerStyles.ts
+++ src/theme/foundations/layerStyles.ts
@@ -1,8 +1,17 @@
+import { useBreakpointValue, useColorModeValue } from '@chakra-ui/react';
+
 const layerStyles = {
   'flex-center': {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  panel: {
+    py: { base: '0', sm: '8' },
+    px: { base: '4', sm: '10' },
+    bg: { base: 'transparent', sm: 'bg-surface' },
+    boxShadow: { base: 'none', sm: 'md' },
+    borderRadius: { base: 'none', sm: 'xl' },
   },
 };
 
