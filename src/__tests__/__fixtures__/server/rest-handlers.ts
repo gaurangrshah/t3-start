c9155070ad245f3e2c96e85b6e210b1a51570df6
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-01 02:46:35 -0500

    wip: msw handlers

--- src/__tests__/__fixtures__/server/rest-handlers.ts
+++ src/__tests__/__fixtures__/server/rest-handlers.ts
@@ -2,6 +2,7 @@
  * @ SEE: https://tinyurl.com/2j29z5mb
  */
 import { rest } from 'msw';
+import { mockSession } from '../../../utils/test/mocks/session';
 export const MOCK_CACHE = 'mock cache';
 export const MOCK_PAGE_ID = 'mock-page-id';
 
@@ -14,6 +15,7 @@ afterEach(() => {
 export const restHandlers = [
   rest.get('*/api/trpc/example.hello', (req, res, ctx) => {
     return res(
+      // ctx.delay(100),
       ctx.status(200),
       ctx.json({
         input: {},
@@ -25,4 +27,22 @@ export const restHandlers = [
       })
     );
   }),
+  rest.get('*/api/auth/session', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: mockSession,
+        status: 'authenticated',
+      })
+    );
+  }),
+  // rest.post('*/api/trpc/auth.register', (req, res, ctx) => {
+  //   res(ctx.status(201));
+  // }),
+  // rest.put('*/api/trpc/auth.register', (req, res, ctx) => {
+  //   res(ctx.status(201));
+  // }),
+  // rest.patch('*/api/trpc/auth.register', (req, res, ctx) => {
+  //   res(ctx.status(201));
+  // }),
 ];
