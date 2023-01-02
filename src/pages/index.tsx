b7409b8121b79640850989e8a18e26c7fb1882f9
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-02 16:16:33 -0500

    add localstorage hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import { Button, chakra, Flex } from '@chakra-ui/react';
-import { randomColor } from '@chakra-ui/theme-tools';
 import { signIn, signOut, useSession } from 'next-auth/react';
 
 import type { ButtonProps } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import type { FC } from 'react';
 
 import { DefaultLayout } from '@/components';
 import { trpc } from '@/utils/trpc';
-import { Session } from 'next-auth';
-import type { FC } from 'react';
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: 'from tRPC' });
