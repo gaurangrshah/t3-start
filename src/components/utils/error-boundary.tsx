174c91baa82ae97909ec76808de4d248bf9257a3
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-03 01:23:57 -0500

    update error boundary

--- src/components/utils/error-boundary.tsx
+++ src/components/utils/error-boundary.tsx
@@ -20,7 +20,7 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', { error, errorInfo });
+    console.error('Error Boundary error:', { error, errorInfo });
   }
 
   public render() {
