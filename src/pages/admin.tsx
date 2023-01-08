00cd51c5ae47a4350c30e70c0d1200417cc1fe4c
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-08 13:06:45 -0500

    reset admin

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,13 +1,10 @@
 import { type NextPageWithAuth } from '@/types';
-import { trpc } from '@/utils/trpc';
 
 // @TODO: build out admin dashboard
 
 const AdminPage: NextPageWithAuth = () => {
-  const { data } = trpc.sandbox.readFile.useQuery({ path: 'package.json' });
-  console.log('🚀 | file: admin.tsx:9 | data', data);
 
-  return <>Admin</>;
+  return <div>Admin</div>;
 };
 
 export default AdminPage;
